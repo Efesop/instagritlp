cc390e5ca41faa2dc37d3ba8eec1fbac1ad9faea
Efesop <128099342+Efesop@users.noreply.github.com>
2025-01-10 19:29:40 +0700

    config updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from 'next'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
 
 const nextConfig: NextConfig = {
   output: isProduction ? 'export' : undefined,
-  basePath: isProduction ? '/instagritlp' : '',
+  basePath: isGitHubPages ? '/instagritlp' : '',
   images: {
     unoptimized: true,
   },
